2d17bbfe0fa38a122876bdb7663a31d0dd079ab0
mcgr0217 <Samantha@samanthas-MacBook-Pro.local>
2015-02-09 18:38:27 -0500

    Var
    Adding the main content to the vars

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var berries = {
+    GrowsOnShrub: '3 tonnes',
+    isMeatEater: Yes,
+    Energy: 240 kJ,
+    Carbohydrates: 14.49g,
+    protein: 0.74g
+};
+
+var grapes = {
+    GrowsOnShrub: No,
+    Energy: 288 kJ,
+    Carbohydrates: 18.1 g,
+    Protein: 0.72 g
+};
+
+var currant = {
+    Grows on shrub: Yes,
+    Energy: 234 kJ,
+    Carbohydrates: 13.8 g,
+    Protein: 1.4 g
+};
+
