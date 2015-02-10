7a0bac8e8c98f3fd142945cf70a17795e7ed8f25
mcgr0217 <Samantha@samanthas-MacBook-Pro.local>
2015-02-09 19:31:54 -0500

    Making the content loop
    Making the content loop by adding the var and deleting the link href
    because you don’t need it on the index page as well adding the main
    content so it generates the answer

--- js/main.js
+++ js/main.js
@@ -1,25 +1,55 @@
-var berries = {
-    name: "Vaccinium corymbosum"
+
+
+var blueberries = {
+    name: "blueberries",
+    latin: "Vaccinium corymbosum",
     GrowsOnShrub: '3 tonnes',
-    isMeatEater: Yes,
-    Energy: 240 kJ,
-    Carbohydrates: 14.49g,
-    protein: 0.74g
+    isMeatEater: true,
+    Energy: 240,
+    Carbohydrates: 14.49,
+    Protein: 0.74
 };
 
 var grapes = {
-    name: "Vitis vinifera",
-    GrowsOnShrub: No,
-    Energy: 288 kJ,
-    Carbohydrates: 18.1 g,
-    Protein: 0.72 g
+    name: "grapes",
+    latin: "Vitis vinifera",
+    GrowsOnShrub: false,
+    Energy: 288,
+    Carbohydrates: 18.1,
+    Protein: 0.72
 };
 
 var currant = {
-    name: "Ribes rubrum",
-    Grows on shrub: Yes,
-    Energy: 234 kJ,
-    Carbohydrates: 13.8 g,
-    Protein: 1.4 g
+    name: "currant",
+    latin: "Ribes rubrum",
+    GrowsOnShrub: true,
+    Energy: 234,
+    Carbohydrates: 13.8,
+    Protein: 1.4
 };
 
+var berry = [blueberries, grapes, currant];
+
+berry.forEach(function (item) {
+ document.write('<dl>');
+    
+    document.write('<dt>name:</dt>');
+ document.write('<dd>' + item.name + '</dd>');
+    
+ document.write('<dt>latin:</dt>');
+ document.write('<dd>' + item.latin + '</dd>');
+    
+document.write('<dt>GrowsOnShrub:</dt>');
+ document.write('<dd>' + item.GrowsOnShrub + '</dd>');
+    
+    document.write('<dt>Energy:</dt>');
+ document.write('<dd>' + item.Energy + '</dd>');
+    
+    document.write('<dt>Carbohydrates:</dt>');          document.write('<dd>' + item.Carbohydrates + '</dd>');
+    
+    document.write('<dt>Protein:</dt>');
+    document.write('<dd>' + item.Protein + '</dd>');
+    
+    
+ document.write('</dl>');
+});
